451aca5e8c9d427b1046f8276fe3b46d0a38322a
Felipe Henrique <felipehfsouza@gmail.com>
2022-05-30 18:30:58 -0300

    feat: ecs migration was finished

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,6 +1,7 @@
 mod player_input;
 mod map_render;
 mod entity_render;
+mod collisions;
 
 use crate::prelude::*;
 
@@ -8,6 +9,7 @@ pub fn build_scheduler() -> Schedule {
     Schedule::builder()
     .add_system(player_input::player_input_system())
     .add_system(entity_render::entity_render_system())
+    .add_system(collisions::collisions_system())
     .add_system(map_render::map_render_system())
     .build()
 }
