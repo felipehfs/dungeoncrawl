5b354e98c75418f5a008078c870d52e8c8ef464c
Felipe Henrique <felipehfsouza@gmail.com>
2022-06-01 07:37:47 -0300

    feat: added hud and tooltips

--- src/main.rs
+++ src/main.rs
@@ -69,6 +69,9 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
         self.resources.insert(ctx.key);
+        
+        ctx.set_active_console(0);
+        self.resources.insert(Point::from_tuple(ctx.mouse_pos()));
 
         let current_state = self.resources.get::<TurnState>().unwrap().clone();
         match current_state {
@@ -76,6 +79,9 @@ impl GameState for State {
             TurnState::PlayerTurn => self.player_systems.execute(&mut self.ecs, &mut self.resources),
             TurnState::MonsterTurn => self.monster_systems.execute(&mut self.ecs, &mut self.resources),
         };
+
+        ctx.set_active_console(2);
+        ctx.cls();
 
         render_draw_buffer(ctx).expect("Render error");
     }
@@ -86,10 +92,13 @@ fn main() -> BError {
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_dimensions(32, 32)
         .with_resource_path("resources/")
         .with_font("dungeonfont.png", 32, 32)
+        .with_font("terminal8x8.png", 8, 8)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, "terminal8x8.png")
         .build()?;
     main_loop(context, State::new())
 }
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -5,6 +5,8 @@ mod collisions;
 mod random_move;
 mod end_turn;
 mod movement;
+mod hud;
+mod tooltips;
 
 use crate::prelude::*;
 
@@ -14,6 +16,8 @@ pub fn build_input_scheduler() -> Schedule {
     .flush()
     .add_system(map_render::map_render_system())
     .add_system(entity_render::entity_render_system())
+    .add_system(hud::hud_system())
+    .add_system(tooltips::tooltips_system())
     .build()
 }
 
@@ -23,6 +27,8 @@ pub fn build_player_scheduler() -> Schedule {
     .flush()
     .add_system(map_render::map_render_system())
     .add_system(entity_render::entity_render_system())
+    .add_system(hud::hud_system())
+    .add_system(tooltips::tooltips_system())
     .add_system(end_turn::end_turn_system())
     .build()
 }
@@ -35,6 +41,7 @@ pub fn build_monster_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
