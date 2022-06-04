731409dff0eba667945e9da0e78b03a00645ebb1
Felipe Henrique <felipehfsouza@gmail.com>
2022-06-03 23:20:35 -0300

    feat: added a amulet of yala and victory state

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,7 @@ impl State {
         let map_builder = MapBuilder::new(&mut rng);
 
         spawn_player(&mut ecs, map_builder.player_start);
+        spawn_amulet_of_yala(&mut ecs, map_builder.amulet_start);
 
         map_builder
             .rooms
@@ -64,6 +65,39 @@ impl State {
         }
     }
 
+    fn reset_game_state(&mut self) {
+        self.ecs = World::default();
+        self.resources = Resources::default();
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+
+        spawn_player(&mut self.ecs, map_builder.player_start);
+        spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
+
+        map_builder
+            .rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
+
+        self.resources.insert(map_builder.map);
+        self.resources.insert(Camera::new(map_builder.player_start));
+        self.resources.insert(TurnState::AwaitingInput);
+    }
+
+    fn victory(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(2);
+        ctx.print_color_centered(2, GREEN, BLACK, "You have won!");
+        ctx.print_color_centered(4, WHITE, BLACK, "You put on the Amulet of Yala and feel its powr course through your veins.");
+        ctx.print_color_centered(5, WHITE, BLACK, "Your town is saved, and you can return to your normal life.");
+        ctx.print_color_centered(7, GREEN, BLACK, "Press 1 to play again.");
+
+        if let Some(VirtualKeyCode::Key1) = ctx.key {
+            self.reset_game_state();
+        }
+    }
+
     fn game_over(&mut self, ctx: &mut BTerm) {
         ctx.set_active_console(2);
         ctx.print_color_centered(2, RED, BLACK, "Your quest has ended.");
@@ -73,26 +107,23 @@ impl State {
             BLACK,
             "Slain by a monster, your hero's journey has come to a premature end.",
         );
-        
-        ctx.print_color_centered(5, WHITE, BLACK, "The amulet of Yala remains unclaimed, and your home town is not saved.");
-        ctx.print_color_centered(8, YELLOW, BLACK, "Don't worry, you can always try again with a new hero.");
+
+        ctx.print_color_centered(
+            5,
+            WHITE,
+            BLACK,
+            "The amulet of Yala remains unclaimed, and your home town is not saved.",
+        );
+        ctx.print_color_centered(
+            8,
+            YELLOW,
+            BLACK,
+            "Don't worry, you can always try again with a new hero.",
+        );
         ctx.print_color_centered(9, GREEN, BLACK, "Press 1 to play again.");
 
-        
-
         if let Some(VirtualKeyCode::Key1) = ctx.key {
-            self.ecs = World::default();
-            self.resources = Resources::default();
-            let mut rng = RandomNumberGenerator::new();
-            let map_builder = MapBuilder::new(&mut rng);
-            spawn_player(&mut self.ecs, map_builder.player_start);
-
-            map_builder.rooms.iter().skip(1).map(|r| r.center())
-                .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
-
-            self.resources.insert(map_builder.map);
-            self.resources.insert(Camera::new(map_builder.player_start));
-            self.resources.insert(TurnState::AwaitingInput);
+            self.reset_game_state();
         }
     }
 }
@@ -124,8 +155,8 @@ impl GameState for State {
                 .monster_systems
                 .execute(&mut self.ecs, &mut self.resources),
             TurnState::GameOver => self.game_over(ctx),
+            TurnState::Victory => self.victory(ctx),
         };
-
 
         render_draw_buffer(ctx).expect("Render error");
     }
--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -2,14 +2,21 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(Health)]
+#[read_component(Point)]
 #[read_component(Player)]
+#[read_component(AmuletOfYala)]
 pub fn end_turn(
     ecs: &SubWorld,
     #[resource] turn_state: &mut TurnState
 ) {
-    let mut player_hp = <&Health>::query().filter(component::<Player>());
+    let mut player_hp = <(&Health, &Point)>::query().filter(component::<Player>());
     let current_state = turn_state.clone();
+    let mut amulet = <&Point>::query()
+        .filter(component::<AmuletOfYala>());
 
+    let amulet_pos = amulet.iter(ecs).nth(0).unwrap();
+
+    
     let mut new_state = match turn_state {
         TurnState::AwaitingInput => return,
         TurnState::PlayerTurn => TurnState::MonsterTurn,
@@ -17,9 +24,13 @@ pub fn end_turn(
         _ => current_state,
     };
 
-    player_hp.iter(ecs).for_each(|hp| {
+    player_hp.iter(ecs).for_each(|(hp, pos)| {
         if hp.current < 1 {
             new_state = TurnState::GameOver;
+        }
+
+        if pos == amulet_pos {
+            new_state = TurnState::Victory;
         }
     });
 
